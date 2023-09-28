d47c0ce2e6a144daa360bcce896b2e0252524a23
VirusAnimates <103518350+Virus01Official@users.noreply.github.com>
2023-09-28 16:01:26 +0200

    Create Gui Button Clicking.lua

---
+++ Gui Button Clicking.lua
@@ -0,0 +1,6 @@
+local UI = script.Parent --if the script is in the GUI Frame
+local button = UI.TextButton --if the button is in the GUI Frame and it's TextButton
+
+button.MouseButton1Click:Connect(function()
+    --do something
+end)
